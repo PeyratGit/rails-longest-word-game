64385103f2f0b0f866efc60003a514f3fcd29e7f
Clement Peyrat <aiglefeu31@gmail.com>
2022-02-14 17:00:39 +0100

    Added score and style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,7 @@ class GamesController < ApplicationController
 
   def score
     letters = params[:letters].split("")
+    letters_unchanged = params[:letters].split("")
     @guess = params[:guess]
     @guess_arr = @guess.split("")
     @api_result = URI.open("https://wagon-dictionary.herokuapp.com/#{@guess}")
@@ -20,11 +21,12 @@ class GamesController < ApplicationController
       end
     end
     if in_grid == false
-      @message = "Sorry but #{@guess} can't be built out of #{letters.join(" | ").upcase}"
+      @message = "Sorry but #{@guess} can't be built out of #{letters_unchanged.join(" | ").upcase}"
     elsif in_grid == true && found == false
       @message = "Sorry but #{@guess} does not seem to be a valid English word"
     else
       @message = "Congratulations! #{@guess} is a valid English word !"
+      session['score'] = (session['score'] || 0) + (@guess.length * 2)
     end
   end
 end
